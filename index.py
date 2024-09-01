e99a7aed98ecf40461ddd9e6feca9740fd2e3bde
Juanjjj124 <Juanmanuelk28@gmail.com>
2024-08-31 21:46:13 -0500

    Update index.py

--- index.py
+++ index.py
@@ -25,3 +25,23 @@ def ver_sala():
             print("")
     else:
         print("La sala no existe.")
+
+salas = {}
+
+def menu():
+    print("\n--- Menú Principal ---")
+    print("1. Crear sala")
+    print("2. Ver sala")
+    print("3. Asignar puesto")
+    print("4. Cargar sala")
+    print("5. Salir")
+    return input("Seleccione una opción: ")
+
+def crear_sala():
+    nombre_sala = input ("Ingrese el nombre de la sala: ")
+    filas = int(input("Ingrese la cantidad de filas: "))
+    columnas = [['0' for _ in range(columnas)] for _ in range(filas)]
+
+    salas[nombre_sala] = salas
+    print(f"Sala '{nombre_sala}' creada exitosamente.")
+  
