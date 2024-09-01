d596d8d827edd829a0883523d4e9623b13dbc439
Juanjjj124 <Juanmanuelk28@gmail.com>
2024-08-31 21:48:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,23 @@
-def ver_sala():
+salas = {}
+
+def menu():
+    print("\n--- Menú Principal ---")
+    print("1. Crear sala")
+    print("2. Ver sala")
+    print("3. Asignar puesto")
+    print("4. Cargar sala")
+    print("5. Salir")
+    return input("Seleccione una opción: ")
+
+def crear_sala():
+    nombre_sala = input ("Ingrese el nombre de la sala: ")
+    filas = int(input("Ingrese la cantidad de filas: "))
+    columnas = [['0' for _ in range(columnas)] for _ in range(filas)]
+
+    salas[nombre_sala] = salas
+    print(f"Sala '{nombre_sala}' creada exitosamente.")
+    
+    def ver_sala():
     nombre_sala = input("Ingrese el nombre de la sala a visualizar: ")
     if nombre_sala in salas:
         sala = salas[nombre_sala]
@@ -26,22 +45,5 @@ def ver_sala():
     else:
         print("La sala no existe.")
 
-salas = {}
 
-def menu():
-    print("\n--- Menú Principal ---")
-    print("1. Crear sala")
-    print("2. Ver sala")
-    print("3. Asignar puesto")
-    print("4. Cargar sala")
-    print("5. Salir")
-    return input("Seleccione una opción: ")
-
-def crear_sala():
-    nombre_sala = input ("Ingrese el nombre de la sala: ")
-    filas = int(input("Ingrese la cantidad de filas: "))
-    columnas = [['0' for _ in range(columnas)] for _ in range(filas)]
-
-    salas[nombre_sala] = salas
-    print(f"Sala '{nombre_sala}' creada exitosamente.")
   
